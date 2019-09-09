a4d2f8381ccd318cd10e5c0878d447fb900111bd
hwd <1105185293@qq.com>
2019-09-09 09:52:33 +0800

    提交到本地仓库周考

---
+++ src/main/java/com/huwande/common/controller/TextController.java
@@ -0,0 +1,16 @@
+package com.huwande.common.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 
+    * @ClassName: TextController
+    * @Description:输入多行带分段的文本，提交到后台Controller控制器中。
+    * @author De.nnn
+    * @date 2019年9月9日
+    *
+ */
+@Controller
+public class TextController {
+
+}
